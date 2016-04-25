f758a0ebf3d338ad3a957ca962bfd0cb65fa4e0b
johaolopztools <johaolopztools@gmail.com>
2016-04-25 01:42:44 -0500

    WebSite PHP

---
+++ include/logo2.php
@@ -0,0 +1,3 @@
+<?php 
+echo '<div id="bar" align="center"><b>WebOS_Un sitio para webear</b><div><br><img src="img/WebOS.png"><div style="clear:both;font-size:0.5em;margin:8px 0 -4px 0;color:white;">Tecnologías WEB</div></div></div>';
+?> 
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+<title>WebOS_App_GoogleCloudPlatform</title>
+<meta name="Description" content="Pagina de prueba para Google Cloud Platform">
+<meta name="viewport" content="initial-scale=1.0, user-scalable=yes">
+<meta name="Robots" content="index, follow">
+<link href="style/estilo.css" rel="stylesheet" type="text/css" media="screen">
+</head> 
+<body>
+<div id="inicio"></div>
+<div id="header">
+<?php    
+    include "include/logo2.php";   
+?>
+<div id="page">
+<h1>Página de los de la WebOpenSource en Google Cloud Platform</h1>
+
+<div class="intro">
+Somos el equipo de desarrollo FrontEnd los WebOS.
+</div>
+Esta es una página de prueba que se ejecuta sobre GCP.<br>
+Podemos apreciar que todo el contenido es administrado directamente desde la nube con cada uno de los recursos y aplicativos de Google Cloud Platform.<br>
+Hemos utilizado los lenguajes de desarrollo vistos en clase como Html5, Css y como valor agregado algo de PHP.<br>
+<img style="margin:12px 0 12px 0;" src="img/hacer-paginas-php.jpg" width="620" height="400" alt="Imagen para el intro" title="WebOS, tecnologias web"><br>
+
+<h2>Conozca nuestros proyectos</h2>
+<div class="enlaces">
+<a href="http://www.coyotexpress.blogea.me/">CoyotExpress WebSite</a><br>
+</div>
+
+Este link demuestra nuestra capacidad de desarrollo FrontEnd, en la que podemos apreciar páginas de: galeria, acerca de, contacto y productos.<br>
+En la parte inferior podrá encontrar nuestro enlace a las redes sociales para que pueda seguirnos y estar al tanto de nuestros proyectos.<br>
+<br>
+
+<?php    
+    include "include/share.php";   
+?> 
+
+<?php    
+    include "include/footer.php";   
+?> 
+</div></div>
+</body> 
+</html>
